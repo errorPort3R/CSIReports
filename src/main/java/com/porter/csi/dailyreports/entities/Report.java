1e86fe58cfb451bb6818092b6329c186df324006
jeffryporter <jporter7@gmail.com>
2017-09-13 16:41:58 -0400

    add report entity and repository.

---
+++ src/main/java/com/porter/csi/dailyreports/entities/Report.java
@@ -0,0 +1,13 @@
+package com.porter.csi.dailyreports.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by jeffryporter on 9/13/17.
+ */
+@Entity
+@Table(name = "reports")
+public class Report
+{
+}
---
+++ src/main/java/com/porter/csi/dailyreports/services/ReportRepository.java
@@ -0,0 +1,8 @@
+package com.porter.csi.dailyreports.services;
+
+/**
+ * Created by jeffryporter on 9/13/17.
+ */
+public interface ReportRepository
+{
+}
